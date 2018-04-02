4a6683791969a266087cf7f557291311becf4305
Niklas Winde <36261146+NikTheWin@users.noreply.github.com>
2018-04-02 21:26:28 +0200

    Adding stuff
    Added filter and some more printouts

--- Tactilis/Program.cs
+++ Tactilis/Program.cs
@@ -13,20 +13,25 @@ namespace Tactilis
         {
             Stopwatch sw = new Stopwatch();
 
-            sw.Start();
-
             int[] outBuf = new int[44100];
+
+            ButterworthLowPassFilter lpf = new ButterworthLowPassFilter();
+            
+            sw.Start();
 
             for (int i = 0; i < 44100; i++)
             {
                 double yValue = Math.Sin(i * 2 * Math.PI);
+
+                lpf.Filter(yValue);
 
                 outBuf[i] = (int)Math.Round(yValue * 44100);
             }
 
             sw.Stop();
 
-            Console.WriteLine("Sine for-loop took: " + sw.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Synth for-loop took: " + sw.ElapsedMilliseconds.ToString());
+            Console.WriteLine("You could have " + (800 / sw.ElapsedMilliseconds).ToString() + " voices of this synth with 20% margin");
             Console.ReadLine();
         }
     }
